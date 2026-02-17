68f718753b3f0bc98fdd5ac1a26cb84a9e1d2613
Nicolas-Bessin <nicolasbessin49@gmail.com>
2026-02-18 00:27:49 +0100

    Initial commit : exact formulations & finding good seeds

---
+++ src/data.jl
@@ -0,0 +1,21 @@
+struct Data
+    N :: Int
+    M :: Int
+    assignment_costs :: Matrix{Int}
+    facility_costs :: Vector{Int}
+end
+
+using Random
+
+l1_norm(p1 :: Vector{Int}, p2 :: Vector{Int}) :: Int = sum(abs.(p1 .- p2))
+l2_norm(p1 :: Vector{Int}, p2 :: Vector{Int}) :: Float64 = sqrt(sum((p1 .- p2) .^ 2))
+
+function random_data(N::Int, M::Int; seed :: Int = 42) :: Data
+    Random.seed!(seed)
+    # Generate N random points in 2D space with int coordinates
+    client_point = rand(0:250, N, 2)
+    facility_points = rand(0:250, M, 2)
+    assignment_costs = [l1_norm(client_point[i, :], facility_points[j, :]) for i in 1:N, j in 1:M]
+    facility_costs = rand(25:50, M)
+    return Data(N, M, assignment_costs, facility_costs)
+end
---
+++ src/exact.jl
@@ -0,0 +1,45 @@
+include("data.jl")
+
+using JuMP, Gurobi
+
+function solve_FL(data :: Data; relaxed :: Bool, verbose = false, env = Gurobi.Env())
+    N = data.N
+    M = data.M
+
+    model = Model(() -> Gurobi.Optimizer(env))
+    if !verbose
+        set_silent(model)
+    end
+
+    # Disable presolve to illustrate the relaxation gap 
+    set_optimizer_attribute(model, "Presolve", 0)
+    @variable(model, y[1:M] >= 0)
+    @variable(model, x[1:N, 1:M] >= 0)
+
+    @constraint(model, [i=1:N], sum(x[i, j] for j in 1:M) == 1)
+    @constraint(model, [i=1:N, j=1:M], x[i, j] <= y[j])
+
+    @objective(model, Min,
+        sum(data.assignment_costs[i, j] * x[i, j] for i in 1:N, j in 1:M) +
+        sum(data.facility_costs[j] * y[j] for j in 1:M)
+    )
+    
+    if !relaxed
+        set_binary.(y)
+    end
+
+    optimize!(model)
+    @assert is_solved_and_feasible(model)
+
+    objective = objective_value(model)
+    assignment_cost = sum(data.assignment_costs[i, j] * value(x[i, j]) for i in 1:N, j in 1:M)
+    assignment_share = assignment_cost / objective
+
+    if verbose
+        println("Mode = ", relaxed ? "Relaxed" : "Integer")
+        println("Optimal objective value: ", objective)
+        println("Assignment share of total cost: ", round(assignment_share * 100, digits=2), "%")
+    end
+
+    return objective, value.(y), value.(x)
+end
